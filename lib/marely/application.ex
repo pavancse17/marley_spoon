deade1d9e621f8ceede70ce30475bcf1a4f131f6
Pavan Kumar <pavankumar.kuppala@tyson.com>
2021-07-14 15:53:53 +0530

    Comment out Ecto repo initialization

--- lib/marely/application.ex
+++ lib/marely/application.ex
@@ -8,7 +8,7 @@ defmodule Marely.Application do
   def start(_type, _args) do
     children = [
       # Start the Ecto repository
-      Marely.Repo,
+      # Marely.Repo, 
       # Start the Telemetry supervisor
       MarelyWeb.Telemetry,
       # Start the PubSub system
